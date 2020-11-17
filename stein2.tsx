b109ec41d84bad16dd3f02672f92f09861d375e8
rincewind <nobody@example.com>
2020-11-17 17:15:00 +0100

    Playing with Tiled Editor

---
+++ stein2.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="stein2" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <properties>
+  <property name="collides" type="bool" value="true"/>
+ </properties>
+ <image source="stein2.png" width="32" height="32"/>
+</tileset>
